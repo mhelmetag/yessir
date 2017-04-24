64db7d2d2230fdad78ed14e5b8c5e2fb36898f4c
Max Helmetag <mhelmetag@gmail.com>
2017-04-23 21:32:08 -0700

    use if let instead of matching

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
 use std::env;
 
 fn main() {
-    let argc = env::args().count();
+    let arg_count = env::args().count();
 
-    if argc == 1 {
+    if arg_count == 1 {
         loop {
             println!("y");
         }
     } else {
         loop {
-            for i in 1..argc {
-                match env::args().nth(i) {
-                    Some(x) => print!("{}", x),
-                    None => continue,
+            for index in 1..arg_count {
+                if let Some(arg) = env::args().nth(index) {
+                    print!("{}", arg);
                 }
             }
 
