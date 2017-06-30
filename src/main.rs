84d9a1c2ec714f9b4aa6186a94439e168d7d054e
Max Helmetag <mhelmetag@gmail.com>
2017-06-29 22:08:14 -0700

    rename arg count

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 use std::env;
 
 fn main() {
-    let arg_count = env::args().count();
+    let args_count = env::args().count();
 
-    if arg_count == 1 {
+    if args_count == 1 {
         loop {
             println!("y");
         }
     } else {
         loop {
-            for index in 1..arg_count {
+            for index in 1..args_count {
                 if let Some(arg) = env::args().nth(index) {
                     print!("{}", arg);
                 }
